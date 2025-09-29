e188fb6ae2bbf2abc1109436fa3c1227240c5a91
MD. BELAL ISLAM <88710521+Belalgit@users.noreply.github.com>
2025-09-29 14:10:45 +0600

    Update restore_example_db.sh

--- restore_example_db.sh
+++ restore_example_db.sh
@@ -11,7 +11,7 @@ DB_PATH="/var/backups/mongodb/example_db_dump/example-db"
 DB_NAME="example-db"
 
 if [ ! -d "$DB_PATH" ]; then
-  echo "❌ Dump folder for $DB_NAME not found at: $DB_PATH"
+  echo "Dump folder for $DB_NAME not found at: $DB_PATH"
   exit 1
 fi
 
@@ -20,6 +20,6 @@ mongorestore \
   --uri="$TARGET_URI/$DB_NAME" \
   --gzip \
   --dir="$DB_PATH" \
-  --drop   # ⚠️ Drops existing collections before restore
+  --drop   # Drops existing collections before restore
 
-echo "✅ Restore completed for database: $DB_NAME into $TARGET_URI"
+echo "Restore completed for database: $DB_NAME into $TARGET_URI"
